0f3661e8f4fa850c04818c5275500248a7e59871
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-08-03 01:31:44 +0400

    fix: add validation
    refactor: remove redundant validation

--- src/rpc.ts
+++ src/rpc.ts
@@ -3,21 +3,50 @@ import { getVpValueByStrategy, rpcError, rpcSuccess } from './helpers/utils';
 
 const router = express.Router();
 
-router.post('/', async function (req, res) {
+// Validation middleware
+function validateRequest(req: express.Request, res: express.Response, next: express.NextFunction) {
   const { method, params, id = 0 } = req.body;
-  const { network, snapshot, strategies } = params || {};
 
-  if (method === 'get_value_by_strategy') {
-    try {
-      const result = await getVpValueByStrategy(network, snapshot, strategies);
-
-      return rpcSuccess(res, result, id);
-    } catch (err) {
-      return rpcError(res, 500, err, id);
-    }
+  if (!method) {
+    return rpcError(res, 400, 'Method is required', id);
   }
 
-  return res.status(400).json({ error: 'Method not found' });
+  if (method !== 'get_value_by_strategy') {
+    return rpcError(res, 400, 'Method not allowed', id);
+  }
+
+  if (!params) {
+    return rpcError(res, 400, 'Params is required', id);
+  }
+
+  const { network, snapshot, strategies } = params;
+
+  if (!network) {
+    return rpcError(res, 400, 'Network is required', id);
+  }
+
+  if (!snapshot) {
+    return rpcError(res, 400, 'Snapshot is required', id);
+  }
+
+  if (!strategies) {
+    return rpcError(res, 400, 'Strategies is required', id);
+  }
+
+  next();
+}
+
+router.post('/', validateRequest, async function (req, res) {
+  const {
+    params: { network, snapshot, strategies },
+    id = 0
+  } = req.body;
+
+  try {
+    return rpcSuccess(res, await getVpValueByStrategy(network, snapshot, strategies), id);
+  } catch (err) {
+    return rpcError(res, 500, err, id);
+  }
 });
 
 export default router;
