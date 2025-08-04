72432bee5686c08b41fe54bc56da17e8b8b1193d
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-08-05 03:39:15 +0400

    fix: throw early error when token address is missing

--- src/strategies/erc20-balance-of.ts
+++ src/strategies/erc20-balance-of.ts
@@ -2,12 +2,18 @@ import { StrategyParams } from './index';
 import { getTokenPriceAtTimestamp } from '../helpers/coingecko';
 import { getTokenDecimals } from '../helpers/token';
 
+const DEFAULT_DECIMAL = 18;
+
 export default async function getValue(
   params: StrategyParams,
   network: number,
   snapshot: number
 ): Promise<number> {
-  const decimals = params.decimals ?? 18;
+  if (!params.address) {
+    throw new Error('Address is required for erc20-balance-of strategy');
+  }
+
+  const decimals = params.decimals ?? DEFAULT_DECIMAL;
 
   const tokenDecimals = await getTokenDecimals(network, params.address);
   const price = await getTokenPriceAtTimestamp(network, params.address, snapshot);
