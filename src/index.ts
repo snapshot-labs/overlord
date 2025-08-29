3dbe801f04899511288dd9b06e95ae2502280828
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-08-29 19:38:14 +0400

    refactor: fix deprecated and lint alert

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   const commit = process.env.COMMIT_HASH || '';
-  const version = commit ? `${pkg.version}#${commit.substr(0, 7)}` : pkg.version;
+  const version = commit ? `${pkg.version}#${commit.slice(0, 7)}` : pkg.version;
   res.json({ version, port: PORT });
 });
 
