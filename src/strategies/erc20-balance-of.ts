5eb4ee12379842aeabd3d619d359f9e920569e86
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-10-09 21:36:46 -0400

    fix: trigger token price fetching first, and return early on empty value

--- src/strategies/erc20-balance-of.ts
+++ src/strategies/erc20-balance-of.ts
@@ -36,14 +36,18 @@ export default async function getValue(
     throw new Error('Address is required for erc20-balance-of strategy');
   }
 
-  const decimals = params.decimals ?? DEFAULT_DECIMAL;
-
-  const tokenDecimals = await getTokenDecimals(network, params.address);
   const price = await getTokenPriceAtTimestamp(
     network,
     params.address,
     snapshot
   );
+
+  if (price === 0) {
+    return 0;
+  }
+
+  const decimals = params.decimals ?? DEFAULT_DECIMAL;
+  const tokenDecimals = await getTokenDecimals(network, params.address);
 
   return price / Math.pow(10, tokenDecimals - decimals);
 }
